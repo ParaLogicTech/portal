4936db9abeeef23d02d6af51296d07eb1f85e320
Saif Ur Rehman <saif@paralogic.io>
2024-03-28 04:14:52 +0500

    style: card shadow

--- frontend/src/components/ItemList/ItemCard.vue
+++ frontend/src/components/ItemList/ItemCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="max-w rounded overflow-hidden shadow-md border border-gray-400 h-full">
+	<div class="max-w rounded overflow-hidden shadow border border-gray-400 h-full">
 		<ItemImage :item="item" class="h-[220px]" />
 		<div class="p-3">
 			<h1 class="font-medium text-sm">{{ item.item_name }}</h1>

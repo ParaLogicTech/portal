d54cc297534ad50fea181e43c2492e718b3ce66e
Saif Ur Rehman <saif@paralogic.io>
2024-04-17 00:37:10 +0500

    fix: csrf_token

--- portal/www/sales_portal.py
+++ portal/www/sales_portal.py
@@ -1,1 +1,17 @@
+import frappe
+from frappe import _
+from frappe.sessions import get_csrf_token
+
 no_cache = 1
+
+
+def get_context():
+	if frappe.session.user == "Guest":
+		frappe.throw(_("Please login first."), frappe.PermissionError)
+
+	csrf_token = get_csrf_token()
+	frappe.db.commit()
+
+	context = frappe._dict()
+	context.csrf_token = csrf_token
+	return context

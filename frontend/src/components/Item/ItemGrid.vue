d3c9fa8f9ca2589e3832910dc0efc489b1209146
Wasiq-Ali <wasiqali878@gmail.com>
2024-04-26 18:07:23 +0500

    refactor: Commonify ItemGrid as well...

--- frontend/src/components/Item/ItemGrid.vue
+++ frontend/src/components/Item/ItemGrid.vue
@@ -1,40 +1,31 @@
 <template>
-	<div class="@container p-3 overflow-y-scroll">
-		<div v-if="loading && !has_data" class="h-full flex items-center justify-center text-gray-700 text-xl">
-			<Spinner class="w-5 mr-2" />
-			<span>Loading Items...</span>
-		</div>
-
-		<div
-			v-else-if="!items?.length"
-			class="h-full flex items-center justify-center text-gray-400 text-xl font-medium"
-		>
-			<CircleSlash class="h-5 w-5 mr-2" />
-			<div>No Items Found</div>
-		</div>
-
-		<div v-else class="grid @md:grid-cols-2 @3xl:grid-cols-3 @5xl:grid-cols-4 @8xl:grid-cols-5 gap-3">
-			<ItemCard
-				v-for="d in items"
-				:key="d.name"
-				:item="d"
-				:matches="get_matches(d)"
-				@item-selected="this.handle_item_selected"
-			/>
-		</div>
-	</div>
+	<GridView
+		:has_data="has_data"
+		:loading="loading"
+		:is_empty="!items?.length"
+		loding="Loading Items..."
+		empty_message="No Items Found"
+	>
+		<ItemCard
+			v-for="d in items"
+			:key="d.name"
+			:item="d"
+			:matches="get_matches(d)"
+			@item-selected="this.handle_item_selected"
+		/>
+	</GridView>
 </template>
 
 <script>
 import ItemCard from "@/components/Item/ItemCard.vue";
-import {CircleSlash} from "lucide-vue-next"
+import GridView from "@/components/Utils/GridView.vue"
 
 export default {
 	name: "ItemGrid",
 
 	components: {
 		ItemCard,
-		CircleSlash,
+		GridView,
 	},
 
 	props: {

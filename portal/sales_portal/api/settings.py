b7920defb3a66448d0cdc14e721228a71a82df10
Saif Ur Rehman <saif@paralogic.io>
2024-05-22 02:55:12 +0500

    fix: missing line

--- portal/sales_portal/api/settings.py
+++ portal/sales_portal/api/settings.py
@@ -10,6 +10,7 @@ def get_settings():
 	out = frappe._dict({
 		"company": settings_doc.company or get_default_company(),
 		"hide_items_without_image": settings_doc.hide_items_without_image,
+		"show_customer_description": settings_doc.show_customer_description,
 		"stock_availability_based_on": settings_doc.stock_availability_based_on,
 		"show_stock_availability": settings_doc.show_stock_availability,
 

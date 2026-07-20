c41f6727875616b374c308a6a2e623f0d6d5cd71
Saif Ur Rehman <saif@paralogic.io>
2026-07-20 14:22:09 +0500

    style(User): move Customer link

--- portal/overrides/user_hooks.py
+++ portal/overrides/user_hooks.py
@@ -15,9 +15,16 @@ def override_user_dashboard(data):
 	data.setdefault("non_standard_fieldnames", {})
 	data["non_standard_fieldnames"]["Customer"] = "user"
 
-	data["transactions"].append({
-		"label": _("Portal"),
-		"items": ["Customer"]
-	})
+	profile_items = ["Customer"]
+
+	ref_section = [d for d in data["transactions"] if d["label"] == _("Profile")]
+	if ref_section:
+		ref_section = ref_section[0]
+		ref_section["items"] = ref_section["items"] + profile_items
+	else:
+		data["transactions"].append({
+			"label": _("Profile"),
+			"items": profile_items,
+		})
 
 	return data
